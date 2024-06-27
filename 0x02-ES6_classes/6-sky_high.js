dafe73174f7eb9e9d1ec406cf33a1abc39c470bb
dayoonasanya <aonasanya@mail.dccc.edu>
2024-06-27 18:50:25 +0000

    Inheritance

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -2,8 +2,18 @@ import Building from './5-building';
 
 export default class SkyHighBuilding extends Building {
   constructor(sqft, floors) {
+    if (typeof floors !== 'number') throw new Error();
     super(sqft);
     this._floors = floors;
+    this._sqft = sqft;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  set sqft(value) {
+    this._sqft = value;
   }
 
   get floors() {
@@ -17,3 +27,4 @@ export default class SkyHighBuilding extends Building {
   evacuationWarningMessage() {
     return `Evacuate slowly the ${this.floors} floors`;
   }
+}
